1a4f8488fb7c27a7fe323b917a1b08b2ef493714
HJHK_MAC <HJHK_MAC@qq.com>
2022-11-20 03:56:09 +0800

    first commit

---
+++ src/day1/test01.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class test01 {
+    public static void main(String[] args) {
+//        java是一种强类型语言
+//        变量
+
+    }
+}
---
+++ src/day2/test02.java
@@ -0,0 +1,7 @@
+package day2;
+
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("常浩");
+    }
+}
