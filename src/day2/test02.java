e23cbf674bce7fe8465c51d3570f00cef6fd9d5a
HJHK_MAC <HJHK_MAC@qq.com>
2022-11-20 09:13:54 +0800

    second commit

--- src/day2/test02.java
+++ src/day2/test02.java
@@ -3,6 +3,6 @@ package day2;
 public class test02 {
     public static void main(String[] args) {
         System.out.println("常浩");
-        System.out.println("拉取请求测试")
+        System.out.println("拉取请求测试");
     }
 }
