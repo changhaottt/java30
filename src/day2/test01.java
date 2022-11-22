5faf30e05c44f1f9370c65e1e0d602e976128ff2
HJHK_MAC <HJHK_MAC@qq.com>
2022-11-22 14:25:30 +0800

    第六次 commit

---
+++ src/day2/test01.java
@@ -0,0 +1,8 @@
+package day2;
+
+public class test01 {
+    public static void main(String[] args) {
+        char a = '常';
+        System.out.println(a);
+    }
+}
--- src/day2/test02.java
+++
@@ -1,8 +0,0 @@
-package day2;
-
-public class test02 {
-    public static void main(String[] args) {
-        System.out.println("常浩");
-        System.out.println("拉取请求测试");
-    }
-}
